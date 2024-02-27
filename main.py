5f7de858c82c2eb9d44d8ad2b472544e368a53aa
Sagar Dalwala <sagardalwala3410@gmail.com>
2024-02-27 22:39:32 +0530

    half commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+import util
+import os
+
+class App:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.main_window.geometry("1200x520+350+100")
+
+        self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login)
+        self.login_button_main_window.place(x=750, y=300)  # Set the Place of the button x - horizontal , y - vertical
+
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
+                                                                    self.register_new_user, fg='black')
+        self.register_new_user_button_main_window.place(x=750, y=400)
+
+        self.webcam_label = util.get_img_label(self.main_window)
+        self.webcam_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_webcam(self.webcam_label)
+
+        self.db_dir = './db'
+        if not os.path.exists(self.db_dir):
+            os.mkdir(self.db_dir)
+
+    def add_webcam(self, label):
+        if 'cap' not in self.__dict__:
+            self.cap = cv2.VideoCapture(0)
+            self.url = "v1.mp4"
+            self.cap.open(self.url)
+
+        self._label = label
+        self.process_webcam()
+
+    def process_webcam(self):
+        ret, frame = self.cap.read()
+
+        if ret:
+            self.most_recent_capture_arr = frame
+            img_ = cv2.cvtColor(self.most_recent_capture_arr , cv2.COLOR_BGR2RGB)
+            most_recent_capture_pil = Image.fromarray(img_)
+            imgtk = ImageTk.PhotoImage(image=most_recent_capture_pil)
+
+            self._label.imgtk = imgtk
+            self._label.configure(image=imgtk)
+
+        self._label.after(20, self.process_webcam)
+
+    def login(self):
+        pass
+
+    def register_new_user(self):
+        self.register_new_user_window = tk.Toplevel(self.main_window)
+        self.register_new_user_window.geometry("1200x520+370+120")
+
+        self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
+        self.entry_text_register_new_user.place(x=750, y=150)
+
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, "Enter Username:")
+        self.text_label_register_new_user.place(x=750, y=70)
+
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, "Accept", 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window.place(x=750, y=300)
+
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, "try again", 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window.place(x=750, y=400)
+
+        self.capture_label = util.get_img_label(self.register_new_user_window)
+        self.capture_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_img_to_label(self.capture_label)
+       
+
+    def try_again_register_new_user(self):
+        self.register_new_user_window.destroy()
+
+    def add_img_to_label(self, label):
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+        label.imgtk = imgtk
+        label.configure(image=imgtk)
+
+        self.register_new_user_capture = self.most_recent_capture_arr.copy()
+
+    def start(self):
+        self.main_window.mainloop()
+
+    
+    def accept_register_new_user(self):
+        name = self.entry_text_register_new_user.get(1.0, "end-1c")
+
+        cv2.imwrite(os.path.join(self.db_dir, '{}.jpg'.format(name)), self.register_new_user_capture)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
